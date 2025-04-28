1ecd386a9f34dd17ad0331802a5a301184c652af
Nafisa Valieva <n.valieva@innopolis.university>
2025-04-28 17:38:10 +0300

    changes in pipeline with yandexgpt

--- src/core/translate.py
+++ src/core/translate.py
@@ -1,9 +1,11 @@
-"""A module that contains Translator class that performs text tanslation using LLM"""
+"""A module that contains Translator class that performs text translation using LLM"""
 
 import logging
 import time
-from typing import Callable
+from typing import Callable, Union, Literal
+from pathlib import Path
 
+import requests
 from datasets import Dataset
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -15,7 +17,8 @@ from src.utils.batching_utils import *
 
 class Translator:
     """
-    A class responsible for translating batches of text data using a language model.
+    A class responsible for translating batches of text data using different language models.
+    Supports both OpenAI and Yandex GPT models.
     """
 
     def __init__(
@@ -26,6 +29,7 @@ class Translator:
         batch_size: int,
         batch_result_dir: str,
         batch_dir: str,
+        model_type: Literal["openai", "yandex_gpt"] = "openai"
     ):
         """Initializes the Translator with configuration parameters.
 
@@ -36,15 +40,24 @@ class Translator:
             batch_size (int): Size of processing batches.
             batch_result_dir (str): Directory to save batch results.
             batch_dir (str): Directory containing input batches.
+            model_type (str): Type of model to use ("openai" or "yandex_gpt").
         """
         self.batch_size = batch_size
         self.batch_result_dir = batch_result_dir
         self.batch_dir = batch_dir
+        self.model_type = model_type
 
-        self.model = self._initialize_model(model_config)
-        self.chain = self._get_chain(example_data, system_message)
+        if model_type == "openai":
+            self.model = self._initialize_openai_model(model_config)
+            self.chain = self._get_openai_chain(example_data, system_message)
+        elif model_type == "yandex_gpt":
+            self.model_config = model_config
+            self.system_message = self._prepare_system_template(
+                example_data, system_message)
+        else:
+            raise ValueError(f"Unsupported model type: {model_type}")
 
-    def _initialize_model(self, config: dict) -> ChatOpenAI:
+    def _initialize_openai_model(self, config: dict) -> ChatOpenAI:
         """Initializes the ChatOpenAI language model.
 
         Args:
@@ -56,14 +69,13 @@ class Translator:
         Returns:
             ChatOpenAI: Initialized language model instance.
         """
-        model = ChatOpenAI(
+        return ChatOpenAI(
             base_url=config["base_url"],
             api_key=config["api_key"],
             model=config["model"],
         )
-        return model
 
-    def _prepate_system_template(self, examples_data: dict, system_message: str) -> str:
+    def _prepare_system_template(self, examples_data: dict, system_message: str) -> str:
         """
         Prepares the prompt template by combining the system message and examples.
 
@@ -81,11 +93,10 @@ class Translator:
             f"Example Result: {json.dumps(example['result'], ensure_ascii=False, indent=2).replace('{', '{{').replace('}', '}}')}"
             for example in examples
         )
-
         return system_template
 
-    def _get_chain(self, examples_data: dict, system_message: str) -> Callable:
-        """Creates the processing chain combining prompt, model and parser.
+    def _get_openai_chain(self, examples_data: dict, system_message: str) -> Callable:
+        """Creates the OpenAI processing chain combining prompt, model and parser.
 
         Args:
             examples_data (dict): Example data for prompt construction.
@@ -94,14 +105,52 @@ class Translator:
         Returns:
             Callable: Configured processing chain ready for invocation.
         """
-        system_template = self._prepate_system_template(
+        system_template = self._prepare_system_template(
             examples_data, system_message)
         parser = StrOutputParser()
         prompt_template = ChatPromptTemplate.from_messages(
             [("system", system_template), ("user", "{text}")]
         )
-
         return prompt_template | self.model | parser
+
+    def _call_yandex_gpt(self, user_input: str) -> str:
+        """Makes a request to Yandex GPT API.
+
+        Args:
+            user_input (str): Input text to process.
+
+        Returns:
+            str: Raw response from Yandex GPT.
+        """
+        prompt = {
+            "modelUri": self.model_config["model_uri"],
+            "completionOptions": {
+                "stream": False,
+                "temperature": 0.6,
+                "maxTokens": "20000"
+            },
+            "messages": [
+                {
+                    "role": "system",
+                    "text": self.system_message
+                },
+                {
+                    "role": "user",
+                    "text": user_input
+                }
+            ]
+        }
+
+        response = requests.post(
+            self.model_config["url"],
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Api-Key {self.model_config['api_key']}"
+            },
+            json=prompt
+        )
+        response.raise_for_status()
+        return response.text
 
     def translate(self, input_dataset: Dataset, ResultSchema: BaseModel, max_retries: int = 3, retry_delay: int = 2) -> list:
         """Processes all batches through the translation pipeline.
@@ -126,10 +175,10 @@ class Translator:
             input_dataset, batch_dir=self.batch_dir, batch_size=self.batch_size
         )
         list_of_results = []
+
         for batch_idx, batch in enumerate(batched_input_dataset):
             print(
                 f"Processing batch {batch_idx + 1}/{len(batched_input_dataset)}...")
-
             batch_results = []
 
             formatted_inputs = [
@@ -143,12 +192,18 @@ class Translator:
             attempt = 0
             while not success and attempt < max_retries:
                 try:
-                    res = (
-                        self.chain.invoke({"text": user_input})
-                        .strip("`")
-                        .strip("json")
-                        .strip()
-                    )
+                    if self.model_type == "openai":
+                        res = (
+                            self.chain.invoke({"text": user_input})
+                            .strip("`")
+                            .strip("json")
+                            .strip()
+                        )
+                    else:
+                        response_text = self._call_yandex_gpt(user_input)
+                        result = json.loads(response_text)
+                        res = result["result"]["alternatives"][0]["message"]["text"].strip(
+                            "`").strip('\n')
 
                     result_list = json.loads(res)
                     assert isinstance(
@@ -163,7 +218,7 @@ class Translator:
                     batch_results = validated_results
                     success = True
 
-                except (json.JSONDecodeError, ValueError, AssertionError) as e:
+                except (json.JSONDecodeError, ValueError, AssertionError, requests.exceptions.RequestException) as e:
                     attempt += 1
                     logging.warning(
                         f"Error on batch #{batch_idx}: {str(e)}. Attempt {attempt} of {max_retries}. Retrying..."
@@ -178,7 +233,7 @@ class Translator:
 
             list_of_results.extend(batch_results)
 
-            # Сохраняем результаты батча только если используется разбиение на батчи
+            # Save batch results if batch size > 0
             if self.batch_size > 0:
                 batch_result_dir = create_directory_if_not_exists(
                     self.batch_result_dir)
@@ -187,5 +242,6 @@ class Translator:
                     base_filename="model_result",
                     batch_num=batch_idx,
                     save_dir=batch_result_dir,
-                )  # Сохраняем промежуточные результаты
+                )
+
         return list_of_results
